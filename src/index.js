3d9870a85ba4b61bb9df7dda3493756dcd273f7f
beppe11 <78246566+beppe11@users.noreply.github.com>
2022-12-17 11:30:56 +0900

    オブジェクトの分割代入

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import "./styles.css";
+/**
+ * オブジェクトの分割代入
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const myProfile = {
+  name: "岩元",
+  age: 51
+};
+
+const { age, name } = myProfile;
+
+const message1 = `私の名前は${name}です。年齢は${age}歳です。`;
+
+console.log(message1);
