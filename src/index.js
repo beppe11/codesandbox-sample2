9a26ae7c19b4fc4b7d2ca0b1f1cc6d7d429cf090
beppe11 <78246566+beppe11@users.noreply.github.com>
2022-12-22 22:04:02 +0900

    mapとfilterによる配列の処理

--- src/index.js
+++ src/index.js
@@ -29,9 +29,23 @@
 // console.log(arr3);
 
 //配列のコピーと結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
-const arr6 = [...arr4]; //同じオブジェクトを参照しない要素のコピー
-console.log(arr6);
-const arr7 = [...arr4, ...arr5]; //配列の結合
-console.log(arr7);
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+// const arr6 = [...arr4]; //同じオブジェクトを参照しない要素のコピー
+// console.log(arr6);
+// const arr7 = [...arr4, ...arr5]; //配列の結合
+// console.log(arr7);
+
+/**
+ * mapとfilterによる配列の処理
+ */
+const nameArr = ["Iwamoto", "John", "Tanaka"];
+nameArr.map((name) => console.log(name));
+const numArr = [1, 2, 3, 4, 5];
+const numArr2 = numArr.map((num) => 100 * num);
+console.log(numArr2);
+// numArr[0] = 123;
+// console.log(numArr);
+// console.log(numArr2);
+nameArr.map((name, idx) => console.log(`${idx + 1}番目は${name}です`));
+console.log(numArr.filter((num) => num % 2 === 1));
